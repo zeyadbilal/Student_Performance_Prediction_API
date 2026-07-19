1851d4271be701800a3462c9c24f8265b3c7fabd
Ashraf Muhammed <bakr29420@gmail.com>
2026-07-19 13:48:40 +0300

    feat: add backend ( express.js, postgres )  that call the model endpoint

---
+++ backend/src/middleware/validate.js
@@ -0,0 +1,11 @@
+const { body, validationResult } = require('express-validator');
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array().map((e) => e.msg) });
+  }
+  next();
+};
+
+module.exports = validate;
---
+++ backend/src/models/User.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: { msg: 'Name is required' },
+      len: { args: [2, 100], msg: 'Name must be 2-100 characters' },
+    },
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: { msg: 'Email already registered' },
+    validate: {
+      isEmail: { msg: 'Invalid email format' },
+    },
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    validate: {
+      len: { args: [6, 255], msg: 'Password must be at least 6 characters' },
+    },
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      user.password = await bcrypt.hash(user.password, 12);
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 12);
+      }
+    },
+  },
+});
+
+User.prototype.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+User.prototype.toJSON = function () {
+  const values = { ...this.get() };
+  delete values.password;
+  return values;
+};
+
+module.exports = User;
